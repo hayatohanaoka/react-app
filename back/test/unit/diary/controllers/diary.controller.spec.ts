7e956e1be2a7108132d29ea0f8f96ee51f4ff659
hayatohanaoka <hayato.hanaoka@uzabase.com>
2025-03-22 14:10:36 +0900

    unit test done: post /api/diaries

--- back/test/unit/diary/controllers/diary.controller.spec.ts
+++ back/test/unit/diary/controllers/diary.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DiaryController } from '../../../../src/diary/controllers/diary.controller';
+import {
+  DiaryController,
+  DiaryRequest,
+} from '../../../../src/diary/controllers/diary.controller';
+import { DiaryService } from '../../../../src/diary/providers/diary.service';
 
 describe('DiaryController', () => {
   let controller: DiaryController;
+  const diaryServiceMock = jest.spyOn(DiaryService.prototype, 'create');
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [DiaryController],
+      providers: [DiaryService],
     }).compile();
 
     controller = module.get<DiaryController>(DiaryController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('POST /api/diaries', () => {
+    it('diary serviceのcreateメソッドを呼び出す', () => {
+      const diaryRequest: DiaryRequest = {
+        title: 'test',
+        content: 'test',
+        date: '2025-03-21',
+      };
+
+      diaryServiceMock.mockImplementationOnce((diaryRequest: DiaryRequest) => {
+        console.log(
+          `mock impl is called by ${diaryRequest.title} ${diaryRequest.content} ${diaryRequest.date}`,
+        );
+        return 201;
+      });
+
+      controller.create(diaryRequest);
+      expect(diaryServiceMock).toHaveBeenCalledWith(diaryRequest);
+    });
   });
 });
